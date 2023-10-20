268dd7c7029deade0796eee385a553932eb7298b
Fabio Santos <fabiosantos@Fabios-MacBook-Pro.local>
2023-10-20 18:06:58 -0300

    feat: add MongoCreateUsersRepository

---
+++ src/repositories/create-users/mongo-create-user.ts
@@ -0,0 +1,25 @@
+import {
+  CreateUserParams,
+  ICreateUsersRepository,
+} from "@/controllers/create-users/protocols"
+import { MongoClient } from "@/database/mongo"
+import { User } from "@/models/user"
+
+export class MongoCreateUser implements ICreateUsersRepository {
+  async createUser(params: CreateUserParams): Promise<User> {
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params)
+
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: insertedId })
+
+    if (!user) {
+      throw new Error("User not created")
+    }
+
+    const { _id, ...rest } = user
+    return { id: _id.toHexString(), ...rest }
+  }
+}
