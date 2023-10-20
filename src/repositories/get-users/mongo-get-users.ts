55478fb79eec7a7adff0b19458751cc2889d6453
Fabio Santos <fabiosantos@Fabios-MacBook-Pro.local>
2023-10-20 17:47:19 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,14 @@
 import { IGetUsersRepository } from "@/controllers/get-users/protocols"
 import { User } from "@/models/user"
+import { MongoClient } from "@/database/mongo"
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Fabio",
-        lastName: "Santos",
-        email: "fabio@gmail.com",
-        password: "1234",
-      },
-    ]
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray()
+
+    return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }))
   }
 }
